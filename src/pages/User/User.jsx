6edb16dcc40f74e3d9a6c70ca5d078516555ac1d
Nirav Mandli <niravmandli462@gmail.com>
2023-07-12 10:10:06 +0530

    Change Color from :orange: to :lemon:

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,12 +1,13 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import moment from 'moment'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
+import { Loading } from '../Home/Home';
 
 const User = () => {
-  const [loading, setLoading] = useState(true)
-  const [details, setDetails] = useState({})
-  const { username } = useParams()
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState({});
+  const { username } = useParams();
 
   useEffect(() => {
     axios({
@@ -15,26 +16,26 @@ const User = () => {
       url: `/user/${username}.json`,
     })
       .then((response) => {
-        setDetails(response.data)
-        setLoading(false)
+        setDetails(response.data);
+        setLoading(false);
       })
       .catch((error) => {
-        console.log(error.response)
-        setLoading(false)
-      })
+        console.log(error.response);
+        setLoading(false);
+      });
     return () => {
-      setDetails({})
-      setLoading(true)
-    }
-  }, [])
+      setDetails({});
+      setLoading(true);
+    };
+  }, []);
 
   return (
     <div className='mx-auto mb-4 max-w-2xl'>
       {loading ? (
-        'Loading...'
+        <Loading />
       ) : (
-        <div>
-          <h1 className='mb-3 text-2xl text-orange-600'>{username}</h1>
+        <>
+          <h1 className='mb-3 text-2xl text-lime-600'>{username}</h1>
           <div className='space-x-1'>
             <span>Karma:</span>
             <span>{details.karma}</span>
@@ -43,10 +44,10 @@ const User = () => {
             <span>Created:</span>
             <span>{moment.unix(details.created).format('LL')}</span>
           </div>
-        </div>
+        </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
