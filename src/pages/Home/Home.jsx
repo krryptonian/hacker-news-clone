93ce435f5c5490fcec75c6c9f801bb490d398fd5
krryptonian <niravmandli462@gmail.com>
2024-05-25 05:23:02 +0530

    UI Update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,49 +1,43 @@
-import parse from 'html-react-parser';
-import moment from 'moment';
-import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { Link } from 'react-router-dom';
-import { default as URLParse } from 'url-parse';
+import parse from 'html-react-parser'
+import moment from 'moment'
+import { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { Link } from 'react-router-dom'
+import { default as URLParse } from 'url-parse'
 
 function getHostname(url) {
-  const parsedUrl = new URLParse(url);
-  let domain = parsedUrl.hostname;
+  const parsedUrl = new URLParse(url)
+  let domain = parsedUrl.hostname
 
   if (domain.startsWith('www.')) {
-    domain = domain.slice(4);
+    domain = domain.slice(4)
   }
 
-  return domain;
+  return domain
 }
 
 const Home = () => {
-  const [stories, setStories] = useState([]);
-  const [page, setPage] = useState(1);
+  const [stories, setStories] = useState([])
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
-    fetchItems();
-  }, []);
+    fetchItems()
+  }, [])
 
   const fetchItems = async () => {
-    const response = await fetch(
-      `https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`
-    );
-    const storyIds = await response.json();
-    const startIndex = (page - 1) * 35;
-    const endIndex = page * 35;
-    const itemsPromises = storyIds
-      .slice(startIndex, endIndex)
-      .map(async (id) => {
-        const itemResponse = await fetch(
-          `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-        );
-        return await itemResponse.json();
-      });
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`)
+    const storyIds = await response.json()
+    const startIndex = (page - 1) * 35
+    const endIndex = page * 35
+    const itemsPromises = storyIds.slice(startIndex, endIndex).map(async (id) => {
+      const itemResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+      return await itemResponse.json()
+    })
 
-    const newItems = await Promise.all(itemsPromises);
-    setStories([...stories, ...newItems]);
-    setPage(page + 1);
-  };
+    const newItems = await Promise.all(itemsPromises)
+    setStories([...stories, ...newItems])
+    setPage(page + 1)
+  }
 
   return (
     <div className='mx-auto mb-4 max-w-3xl'>
@@ -60,57 +54,46 @@ const Home = () => {
               <a
                 target='_blank'
                 href={story.url}
-                className='text-lime-500 visited:font-medium visited:text-pink-500 hover:text-lime-400'
+                className='text-blue-600 visited:font-medium visited:text-pink-500 hover:text-blue-800'
               >
                 {story.title}
               </a>
-              <span className='text-xs text-neutral-400'>
-                ({getHostname(story.url)})
-              </span>
+              <span className='text-xs text-neutral-400'>({getHostname(story.url)})</span>
             </div>
-            <div className='flex items-center justify-between text-sm'>
-              <span className='text-neutral-500'>
+            <div className='flex items-center justify-between text-sm text-gray-500'>
+              <span>
                 {story.score} point by&nbsp;
-                <Link
-                  to={`/user/${story.by}`}
-                  className='self-end text-neutral-400 hover:underline'
-                >
+                <Link to={`/user/${story.by}`} className='self-end hover:underline'>
                   {story.by}
                 </Link>
                 &nbsp;at&nbsp;{moment.unix(story.time).format('LLL')}
               </span>
             </div>
+
             {story.text && (
-              <span className='block text-neutral-400'>
-                {parse(story.text)}
-              </span>
+              <div className='my-4 border-l-4 border-blue-100 pl-4'>
+                <div className='prose max-w-none'>{parse(story.text)}</div>
+              </div>
             )}
           </div>
         ))}
       </InfiniteScroll>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
 
 export const Loading = () => {
   return (
-    <div className='flex items-center justify-center gap-3 py-4 text-neutral-400'>
+    <div className='flex items-center justify-center gap-3 py-4 text-gray-400'>
       <svg
-        className='h-5 w-5 animate-spin text-lime-600'
+        className='h-5 w-5 animate-spin text-blue-600'
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
         viewBox='0 0 24 24'
       >
-        <circle
-          className='opacity-25'
-          cx='12'
-          cy='12'
-          r='10'
-          stroke='currentColor'
-          strokeWidth='4'
-        />
+        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
         <path
           className='opacity-75'
           fill='currentColor'
@@ -119,5 +102,5 @@ export const Loading = () => {
       </svg>
       <span className='text-sm'>Loading...</span>
     </div>
-  );
-};
+  )
+}
